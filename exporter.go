24e458c7ed43626f445c46c84fdd8fa7e04d547b
Christoffer Gunning <christoffer.gunning@bonniernews.se>
2018-05-14 11:22:10 +0200

    adding stage 3

--- exporter.go
+++ exporter.go
@@ -41,7 +41,7 @@ var (
 
 func collectAll() error {
 	var errs error
-	for s := 0; s <= 2; s++ { // TODO: Determine max available stage (team 404s for future stages)
+	for s := 0; s <= 3; s++ { // TODO: Determine max available stage (team 404s for future stages)
 		err := collectUser(s)
 		if err != nil {
 			errs = multierror.Append(errs, err)
